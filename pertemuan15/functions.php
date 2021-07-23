530fdf43a85b1955fb6f17e83d56666ba920d6f1
Aziz Kurniawan <aziz.kurniawan13@gmail.com>
2021-07-23 14:08:27 +0700

    update files

--- pertemuan15/functions.php
+++ pertemuan15/functions.php
@@ -92,23 +92,6 @@ function hapus($id)
     return mysqli_affected_rows($conn);
 }
 
-// function ubah($data)
-// {
-//     global $conn;
-//     $id = $data["id"];
-//     $nrp = htmlspecialchars($data["nrp"]);
-//     $nama = htmlspecialchars($data["nama"]);
-//     $email = htmlspecialchars($data["email"]);
-//     $jurusan = htmlspecialchars($data["jurusan"]);
-
-//     $gambarLama = htmlspecialchars($data["gambarLama"]);
-//     //cek apakah user pilih gambar baru atau tidak
-//     if ($_FILES['gambar']['error'] === 4) {
-//         $gambar = $gambarLama;
-//     } else {
-//         $gambar = upload();
-//     }
-
 function ubah($data) {
     global $conn;
     $id = $data["id"];
